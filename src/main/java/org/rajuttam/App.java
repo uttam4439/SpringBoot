fa46ea6fce008f3956be64531b146abc008eaadf
UTTAM KUMAR <rajuttam4439@gmail.com>
2025-09-20 02:05:45 +0530

    setter Injection

--- src/main/java/org/rajuttam/App.java
+++ src/main/java/org/rajuttam/App.java
@@ -11,10 +11,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        // ApplicationContext create a container for me
+        /* ApplicationContext create a container for me
+         when this line comes it create object for all class from configuration in xml file */
+
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        // when this line comes it create object for that class from configuration in xml file
+
         Student s1 = (Student) context.getBean("student");
         s1.work();
+
+        /* "By default " Singleton scope means Spring creates only one bean instance per container,
+         and the same instance is shared across all references and injections.
+
+       2. A new bean instance is created every time you request it from the Spring container
+       (getBean() or injection). --> beans are not shared. every bean can be different data
+       (no automatic destruction).
+
+         */
     }
 }
--- src/main/java/org/rajuttam/Student.java
+++ src/main/java/org/rajuttam/Student.java
@@ -2,6 +2,10 @@ package org.rajuttam;
 
 public class Student {
 
+    public Student(){  // constructor when object is created cons. is called
+        System.out.print("Object Created");
+    }
+
     public void work(){
         System.out.print("Great Package");
     }
